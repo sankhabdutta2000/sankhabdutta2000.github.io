bce13cd4067831a2c063e049ab29c7a5a4fad78b
SANKHABRATA DUTTA <155755526+sankhabdutta2000@users.noreply.github.com>
2024-03-09 03:28:31 +0530

    Add files via upload

---
+++ JQ/logic.js
@@ -0,0 +1,31 @@
+$("document").ready(function () {
+    $("#whatsap").click(function(){
+        URL("")
+        
+    })
+    $("#facebook").click(function () {
+        URL("")
+    })
+    $("#instagram").click(function(){
+        URL("")
+    })
+    $("#telegram").click(function(){
+        URL("")
+        document.write("loading error...");
+    })
+    $("#twiter").click(function(){
+        URL("")
+    })
+    $("#youtube").click(function(){
+        URL("")
+    })
+    $("#linkedin").click(function () {
+        URL("")
+    })
+    $("#gmail").click(function () {
+        URL("")
+    })
+    $("git").click(function(){
+        URL("")
+    })
+})
